225ca46f04880ac36158b0a2040a7674ea273231
Sridhar0435 <ssri668@gmail.com>
2020-04-11 16:54:09 +0530

    Updates

---
+++ precache-manifest.ada297dcf10488354b0af8fd2cffeb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea5961714d12ab97c676f8df07dffd8",
+    "url": "/dkartshoppingcart/index.html"
+  },
+  {
+    "revision": "945a6717d83837ce3034",
+    "url": "/dkartshoppingcart/static/css/main.f0fa48ff.chunk.css"
+  },
+  {
+    "revision": "c2189babc0857d5fdf83",
+    "url": "/dkartshoppingcart/static/js/2.dbdb5f1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dkartshoppingcart/static/js/2.dbdb5f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945a6717d83837ce3034",
+    "url": "/dkartshoppingcart/static/js/main.9e9f595e.chunk.js"
+  },
+  {
+    "revision": "9fbf34594fadd97d6431",
+    "url": "/dkartshoppingcart/static/js/runtime-main.21785dad.js"
+  }
+]);
